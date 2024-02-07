d24d38ac80bdee89370b59d856e9ad83c35cb982
juhyeon <juhyeon1114@gmail.com>
2024-02-07 20:48:28 +0900

    init

---
+++ src/main/java/study/springoauth2authserver/SpringOauth2AuthServerApplication.java
@@ -0,0 +1,13 @@
+package study.springoauth2authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2AuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/springoauth2authserver/SpringOauth2AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springoauth2authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
