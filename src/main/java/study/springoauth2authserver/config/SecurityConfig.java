a827c8eab68d44e7c1939af0e0e754b1cf5ad0f3
juhyeon <juhyeon1114@gmail.com>
2024-03-10 21:12:54 +0900

    AccessToken 커스텀

--- src/main/java/study/springoauth2authserver/config/SecurityConfig.java
+++ src/main/java/study/springoauth2authserver/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
@@ -21,14 +23,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+import study.springoauth2authserver.entity.Authority;
+import study.springoauth2authserver.entity.User;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -129,6 +137,22 @@ public class SecurityConfig {
         return web -> web.debug(false)
                 .ignoring()
                 .requestMatchers("/webjars/**", "/images/**", "/css/**", "/assets/**", "/favicon.ico");
+    }
+
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> jwtTokenCustomizer(UserDetailsService userDetailsService) {
+        return (context) -> {
+            OAuth2TokenType tokenType = context.getTokenType();
+            if (OAuth2TokenType.ACCESS_TOKEN.equals(tokenType)) {
+                String username = context.getPrincipal().getName();
+                User user = (User) userDetailsService.loadUserByUsername(username);
+                List<Authority> authorities = user.getAuthorities();
+                context.getClaims().claims((claims) -> {
+                    claims.put("id", user.getId());
+                    claims.put("authorities", authorities.stream().map(Authority::getAuthority).collect(Collectors.toList()));
+                });
+            }
+        };
     }
 
 }
